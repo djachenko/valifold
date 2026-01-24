53d1ac6adcc67e6e35fdb2957a045ba69cd53ea0
Игорь Дьяченко <i.s.djachenko@gmail.com>
2026-01-25 00:55:37 +0700

    Started validators' tests

---
+++ tests/test_validators.py
@@ -0,0 +1,51 @@
+import tempfile
+from pathlib import Path
+
+import pytest
+
+from src.errors import MandatoryMissedError
+from src.functions import file
+from src.pattern import w
+
+
+@pytest.fixture
+def temp_dir():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        yield Path(tmpdir)
+
+
+def test_file_exists(temp_dir):
+    name = "test.txt"
+
+    test_file = temp_dir / name
+    test_file.touch()
+
+    struct = file(w(name))
+
+    assert not struct.validate_as_root(test_file)
+
+
+def test_file_doesnt_exist_mandatory(temp_dir):
+    name = "test.txt"
+
+    test_file = temp_dir / name
+
+    struct = file(w(name))
+
+    result = struct.validate_as_root(test_file)
+
+    assert result
+    assert any(isinstance(error, MandatoryMissedError) for error in result)
+
+
+def test_file_doesnt_exist_optional(temp_dir):
+    name = "test.txt"
+
+    test_file = temp_dir / name
+
+    struct = file(
+        w(name),
+        is_mandatory=False
+    )
+
+    assert not struct.validate_as_root(test_file)
